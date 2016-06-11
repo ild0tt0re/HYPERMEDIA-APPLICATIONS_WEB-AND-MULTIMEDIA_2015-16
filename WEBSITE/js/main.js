cdd0944f5310db58fa2f02ca8fc4b84a2625fb0f
Angelo Annunziata <aannunziata@byte-code.com>
2016-06-11 11:41:10 +0200

    [HOME] add carousel-control with key

--- WEBSITE/js/main.js
+++ WEBSITE/js/main.js
@@ -1,1 +1,12 @@
 
+/*
+  key-control for carousel
+*/
+$(document).on('keyup', function(e) {
+    if(e.which == 39){
+        $('.carousel').carousel('next');
+    }
+    else if(e.which == 37){
+        $('.carousel').carousel('prev');
+    }
+});
