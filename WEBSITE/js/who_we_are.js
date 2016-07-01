9ec1fddda84c5ed405a56cf868047817dcd22ebf
Angelo Annunziata <aannunziata@byte-code.com>
2016-07-01 13:02:57 +0200

    add header and content for all tab

--- WEBSITE/js/who_we_are.js
+++ WEBSITE/js/who_we_are.js
@@ -1,10 +1,33 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-    $('[href=#innovation]').addClass('active');
-
-    $('.menu-left [data-toggle="tab"]').on('click', '',function(){
+    //handle active-menu item
+    $('.menu-left [data-toggle="tab"]').on('click', '', function () {
         $('.menu-left [data-toggle="tab"]').removeClass('active');
         $(this).addClass('active');
-    });
 
+        //set right header
+        var attr = $(this).attr('href');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            if (attr === "#innovation") {
+                $('#header').attr('src', 'img/header/wwa1-header.jpg');
+                $('#titolo').text('TIM e il futuro');
+                $('#intro_banner').text('Il futuro firmato TELECOM ITALIA')
+
+            } else if (attr === "#testimonials") {
+                $('#header').attr('src', 'img/header/wwa2-header.jpg');
+                $('#titolo').text('Testimonial');
+                $('#intro_banner').text('Le loro opinioni su di noi')
+
+            } else if (attr === "#projects") {
+                $('#header').attr('src', 'img/header/wwa3-header.jpg');
+                $('#titolo').text('Progetti');
+                $('#intro_banner').text('I nostri progetti innovativi')
+
+            } else if (attr === "#contact-us") {
+                $('#header').attr('src', 'img/header/wwa4-header.jpg');
+                $('#titolo').text('Contattaci');
+                $('#intro_banner').text('Vieni a trovarci, ti aspettiamo')
+            }
+        }
+    });
 });
